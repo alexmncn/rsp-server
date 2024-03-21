0ba9baa83b545552820499dae2b82db20c38df62
Alexmncn78 <alejandro10montes@gmail.com>
2024-03-21 03:33:21 +0100

    Feature mostrar/Ocultar contraseña completada

--- static/js-scripts/cg_creds.js
+++ static/js-scripts/cg_creds.js
@@ -1,3 +1,5 @@
+var sdata;
+
 $(document).ready(function() {
     $('#cargar').on('click', function() {
         // Realizar la solicitud AJAX al servidor para escanear la red
@@ -6,6 +8,7 @@ $(document).ready(function() {
             type: 'GET',
             dataType: 'json',
             success: function(data) {
+                sdata = data;
                 // Limpiar la tabla antes de agregar nuevos datos
                 $('#credenciales').empty();
                 // Verificar si data está definido y es iterable
@@ -13,12 +16,24 @@ $(document).ready(function() {
                   // Agregar datos escaneados a la tabla
                   console.log(data);
                   data.forEach(function(credencial) {
+                    
+                    // Rellenamos la variable user en caso de que este vacío
                     if ((credencial.user === '') || (credencial.user === ' ')){
-                      credencial.user = '----------';
+                      credencial.user = '-';
+                    }
+                    
+                    // Definimos los ids de los elementos en relacion al id de la credencial
+                    var icon_id = `icon_${credencial.id}`;
+                    var p_id = `p_${credencial.id}`;
+                    
+                    // Creamos una contraseña oculta con el mismo numero de caracteres que la original
+                    var hide_password = '';
+                    for (var i = 0; i < credencial.password.length; i++) {
+                      hide_password += "•";
                     }
 
                     $('#credenciales').append(`
-                        <div class="tarjeta-cred">
+                        <div id="${credencial.id}" class="tarjeta-cred">
                           <div class="estatico">
                             <p>Sitio Web:</p>
                             <p>Usuario:</p>
@@ -31,7 +46,10 @@ $(document).ready(function() {
                             <p>${credencial.site}</p>
                             <p>${credencial.user}</p>
                             <p>${credencial.email}</p>
-                            <p>${credencial.password}</p>
+                            <div class="password">
+                              <span id="${icon_id}" class="material-symbols-outlined switch-password-icon" onclick="switch_password('${credencial.id}','${icon_id}','${p_id}')">visibility</span>
+                              <p id="${p_id}">${hide_password}</p>
+                            </div>
                             <p>${credencial.description}</p>
                           </div>
                         </div>
@@ -48,14 +66,40 @@ $(document).ready(function() {
     });
 });
 
-function show_password() {
-  if (credenciales.classList.contains('ocultar')){
-    credenciales.classList.remove('ocultar');
-    sp_1.textContent = "visibility_off";
-  } else { 
-    credenciales.classList.add('ocultar');
-    sp_1.textContent = "visibility";
-  }
 
+function switch_password(id, icon_id, p_id){
+  var icon = document.getElementById(icon_id);
+  var p = document.getElementById(p_id);
+
+  if (icon.textContent == "visibility") {
+    var password = "";
+
+    sdata.forEach(function(credencial){
+      if (credencial.id == id) {
+        password = credencial.password;
+      }
+    });
+
+    p.textContent = password;
+    icon.textContent = "visibility_off";
+    icon.classList.add("icon_visibility_off");
+  } else if (icon.textContent == "visibility_off") {
+    var hide_password = "";
+    var password_len = 8; //default
+
+    sdata.forEach(function(credencial) {
+      if (credencial.id == id) {
+        password_len = credencial.password.length;
+      }
+    });
+
+    for (var i = 0; i < password_len; i++) {
+      hide_password += "•";
+    }
+      
+    p.textContent = hide_password;
+    icon.textContent = "visibility";
+    icon.classList.remove("icon_visibility_off");
+  }
 
 }
